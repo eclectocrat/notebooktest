232f59e54cd4c99a99f7773651ae1804eae2aa94
Jeremy Jurksztowicz <jurksztowicz@gmail.com>
2019-07-22 14:53:38 -0400

    added py

---
+++ Untitled.py
@@ -0,0 +1,80 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.1.7
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# +
+# Initialize the plot
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 12))
+
+# Plot the data
+ax1.bar([1.5, 2, 3.5], [3, 4, 6])
+ax2.barh([0.5, 1, 2.5], [0, 1, 6])
+
+# Show the plot
+plt.show()
+# -
+
+print("echo")
+
+# +
+# Initialize the plot
+fig = plt.figure()
+ax1 = fig.add_subplot(131)
+ax2 = fig.add_subplot(132)
+ax3 = fig.add_subplot(133)
+
+# Plot the data
+ax1.bar([1,2,3],[3,4,5])
+ax2.barh([0.5,1,2.5],[0,1,2])
+ax2.axhline(0.45)
+ax1.axvline(0.65)
+ax3.scatter(np.linspace(0, 1, 5), np.linspace(0, 5, 5))
+
+# Delete `ax3`
+fig.delaxes(ax3)
+
+# Show the plot
+plt.show()
+
+# +
+# Prepare the data
+x = np.linspace(0, 10, 100)
+
+# Plot the data
+plt.plot(x, x, label='linear')
+
+# Add a legend
+plt.legend()
+
+# Show the plot
+plt.show()
+
+# +
+# Create a Figure
+fig = plt.figure()
+
+# Set up Axes
+ax = fig.add_subplot(111)
+
+# Scatter the data
+ax.scatter(np.linspace(0, 1, 5), np.linspace(0, 5, 5))
+
+# Show the plot
+plt.show()
+# -
+
+
